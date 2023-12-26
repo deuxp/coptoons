a22040b78ac160cc3630da61f1520c983d7dd9ab
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-12-26 17:52:38 -0500

    yay

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,7 @@
-this is our completely amazing theme.
+<?php 
+  function greet($name, $color) {
+    echo "<p>my name is $name my favourite color is $color.</p>";
+  }
+  greet('friedi', 'green');
+  greet('dorian', 'gray');
+?>
